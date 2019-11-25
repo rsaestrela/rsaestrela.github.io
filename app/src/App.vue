d481bfd168a25610e1b1de5a33ad7cb8109ebd0e
raul.estrela <rsaestrela@gmail.com>
2019-11-25 21:51:47 +0100

    hardcode SEO tags; removed vue-meta

--- app/src/App.vue
+++ app/src/App.vue
@@ -7,68 +7,6 @@
 <script>
   export default {
     name: 'App',
-    data() {
-      return {
-        name: 'Raul Estrela',
-        siteDescription: '@rsaestrela personal website',
-        profileImage: `${location.host}/img/profile_image.png`
-      }
-    },
-    metaInfo() {
-      return {
-        title: 'Raul Estrela',
-        meta: [
-          {
-            'http-equiv': 'Content-Type', 
-            content: 'text/html; charset=utf-8'
-          },
-          {
-            name: 'viewport', 
-            content: 'width=device-width, initial-scale=1'
-          },
-          {
-            name: 'description', 
-            content: this.siteDescription
-          },
-          {
-            property: 'og:title', 
-            content: this.name
-          },
-          {
-            property: 'og:site_name', 
-            content: this.name
-          },
-          {
-            property: 'og:type', 
-            content: 'website'
-          },
-          {
-            property: 'og:url', 
-            content: location.host
-          },
-          {
-            property: 'og:image', 
-            content:  this.profileImage
-          },
-          {
-            property: 'og:description', 
-            content: this.siteDescription
-          },
-          {
-            itemprop: 'name', 
-            content: this.name
-          },
-          {
-            itemprop: 'description', 
-            content: this.siteDescription
-          },
-          {
-            itemprop: 'image', 
-            content: this.profileImage
-          }
-        ]
-      }
-    }
   }
 </script>
 
