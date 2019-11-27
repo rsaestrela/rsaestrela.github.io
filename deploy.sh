40bdf0fea03f70ba8ba26d403273bed4c1d2101d
raul.estrela <rsaestrela@gmail.com>
2019-11-27 21:33:15 +0100

    adjust deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-rm -r css img js index.html
+rm -r css img js index.html favicon.ico
 cd app
 npm run build
 cd dist
@@ -6,6 +6,7 @@ cp -r css ../../
 cp -r img ../../
 cp -r js ../../
 cp index.html ../../
+cp favicon.ico ../../
 git add -A
 git commit -m "automated deployment"
 git push
